fad4039e41ddfd88ee06efddb9348282210f9416
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-01-10 22:54:31 +0530

    fix:Vite url

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; 
 
 import '../assets/CSS/loginForm.css';
+
 const LoginForm = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate(); 
 
   const handlePhoneNumberChange = (e) => {
     setPhoneNumber(e.target.value);
@@ -14,11 +16,36 @@ const LoginForm = () => {
     setPassword(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     // Perform login logic here
-    console.log('Phone Number:', phoneNumber);
-    console.log('Password:', password);
+    const url = import.meta.env.VITE_LOCALHOST_URL ;
+    const body = {
+      phoneNumber,
+      password,
+    };
+   
+    try {
+      const response = await fetch(`${url}/api/login-user`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // If login is successful, navigate to the Product page
+        navigate('/product');
+      } else {
+        console.error('Login failed:', data.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
@@ -42,7 +69,7 @@ const LoginForm = () => {
         />
       </div>
       <button type="submit">Login</button>
-      {/* Add Link to navigate to Register */}
+
       <div>
         Don't have an account? <Link to="/signup">Register here</Link>
       </div>
